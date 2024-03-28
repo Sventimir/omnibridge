521818a2825fcf9994fc90540bad7ab84fad10df
Sventimir <svantimir@gmail.com>
2024-03-28 18:01:06 +0100

    Reformat table.rs.

--- src/lib/data/table.rs
+++ src/lib/data/table.rs
@@ -4,83 +4,83 @@ use super::display::Display;
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, FromPrimitive)]
 pub enum Dir {
-  North = 0,
-  East = 1,
-  South = 2,
-  West = 3
+    North = 0,
+    East = 1,
+    South = 2,
+    West = 3
 }
 
 impl Display for Dir {
-  fn display(&self) -> String {
-    match *self {
-      Dir::North => "N".to_string(),
-      Dir::East => "E".to_string(),
-      Dir::South => "S".to_string(),
-      Dir::West => "W".to_string()
-    }
+    fn display(&self) -> String {
+        match *self {
+            Dir::North => "N".to_string(),
+            Dir::East => "E".to_string(),
+            Dir::South => "S".to_string(),
+            Dir::West => "W".to_string()
+        }
   }
 
-  fn show(&self) -> String {
-    self.display()
-  }
+    fn show(&self) -> String {
+        self.display()
+    }
 }
 
 impl Dir {
-  pub fn from_int(i : u8) -> Dir {
-    FromPrimitive::from_u8(i).unwrap()
-  }
+    pub fn from_int(i : u8) -> Dir {
+        FromPrimitive::from_u8(i).unwrap()
+    }
 
-  pub fn opponent_of(&self, other: &Dir) -> bool {
-    (*self as u8) | 0x1 != (*other as u8) | 0x1
-  }
+    pub fn opponent_of(&self, other: &Dir) -> bool {
+        (*self as u8) | 0x1 != (*other as u8) | 0x1
+    }
 }
 
 pub const DIRS : [Dir; 4] = [Dir::North, Dir::East, Dir::South, Dir::West];
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, FromPrimitive)]
 pub enum Side {
-  NS = 0,
-  EW = 1
+    NS = 0,
+    EW = 1
 }
 
 impl Display for Side {
-  fn display(&self) -> String {
-    match *self {
-      Side::NS => "NS".to_string(),
-      Side::EW => "EW".to_string()
+    fn display(&self) -> String {
+        match *self {
+            Side::NS => "NS".to_string(),
+            Side::EW => "EW".to_string()
+        }
     }
-  }
 
-  fn show(&self) -> String {
-    self.display()
-  }
+    fn show(&self) -> String {
+        self.display()
+    }
 }
 
 impl Side {
-  pub fn from_int(i : u8) -> Side {
-    FromPrimitive::from_u8(i).unwrap()
-  }
+    pub fn from_int(i : u8) -> Side {
+        FromPrimitive::from_u8(i).unwrap()
+    }
 }
 
 #[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub enum Vulnerability {
-  None,
-  NS,
-  EW,
-  Both
+    None,
+    NS,
+    EW,
+    Both
 }
 
 impl Display for Vulnerability {
-  fn display(&self) -> String {
-    match *self {
-      Vulnerability::None => "None".to_string(),
-      Vulnerability::NS => "NS".to_string(),
-      Vulnerability::EW => "EW".to_string(),
-      Vulnerability::Both => "Both".to_string()
+    fn display(&self) -> String {
+        match *self {
+            Vulnerability::None => "None".to_string(),
+            Vulnerability::NS => "NS".to_string(),
+            Vulnerability::EW => "EW".to_string(),
+            Vulnerability::Both => "Both".to_string()
+        }
     }
-  }
 
-  fn show(&self) -> String {
-    self.display()
-  }
+    fn show(&self) -> String {
+        self.display()
+    }
 }
