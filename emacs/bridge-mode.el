e26d33722602b72fe06bd05fcafb3cab3d73d532
Sventimir <svantimir@gmail.com>
2024-05-02 09:17:54 +0200

    Adapt Emacs client to the server's new interface.

--- emacs/bridge-mode.el
+++ emacs/bridge-mode.el
@@ -15,13 +15,29 @@
   :type 'string
   :group 'bridge)
 
-(defvar bridge-mode-callback-queue nil
-  "Queue of callbacks to be called when the server sends a message.")
+(defvar bridge-mode-response-handlers (make-hash-table :test 'equal)
+  "Mapping from request identifiers to callbacks meant to handle responses to these requests.")
+
+(defun bridge-mentor ()
+  "Open a new buffer connected to a bridge-mentor server instance."
+  (interactive)
+  (pop-to-buffer "*bridge-mentor*")
+  (let ((proc (get-buffer-process "*bridge-mentor*")))
+    (if proc (delete-process proc)))
+  (bridge-mode))
+
+(defun bridge-mode-send-request (request callback)
+  "Send the REQUEST to server and register CALLBACK as response handler."
+  (let ((msg (format "%s\n" request)))
+    (progn
+      (puthash (secure-hash 'sha256 msg) callback bridge-mode-response-handlers)
+      (process-send-string nil msg))))
 
 (define-derived-mode bridge-mode special-mode
   "Bridge Mentor client."
-  (erase-buffer)
-  (add-to-list 'bridge-mode-callback-queue 'bridge-mode-greeting t (lambda (_ _) nil))
+  (let ((inhibit-read-only t))
+    (erase-buffer))
+  (clrhash bridge-mode-response-handlers)
   (make-process :name bridge-mentor-server-process
                 :buffer (current-buffer)
                 :command (list bridge-mode-server-bin)
@@ -32,27 +48,28 @@
   "Pass MSG to the first callback from the queue; ignore PROCESS."
   (let ((inhibit-read-only t))
     (condition-case e
-        (let ((expr (read msg))
-              (callback (pop bridge-mode-callback-queue)))
+        (let* ((expr (read msg))
+               (request-id (symbol-name (cadr (assoc 'request_id expr))))
+               (callback (gethash request-id bridge-mode-response-handlers))
+               (result (or (assoc 'error expr) (assoc 'ok expr))))
           (progn
-            (cond ((eq (car expr) 'Error) (message expr))
-                  ((eq (car expr) 'Ok)
-                   (funcall callback (cadr expr))))))
+            (cond ((eq (car result) 'err) (message result))
+                  ((eq (car result) 'ok)
+                   (funcall callback (cadr result))))
+            (remhash request-id bridge-mode-response-handlers)))
       ('error (message (string-trim msg))))))
 
-(defun bridge-mode-greeting (msg)
-  "Handle the greeting MSG from the bridge-mentor server."
-  (message msg))
-
 (defun bridge-mode-insert-board (resp)
-  "Insert the board from RESP into the buffer."
-  (bridge-board-display resp))
+  "Insert the board from RESP at the end of the current buffer."
+  (let ((inhibit-read-only t))
+    (goto-char (point-max))
+    (insert "\n")
+    (bridge-board-display resp)))
 
 (defun bridge-mode-insert-random-board (number)
   "Insert a random bridge board with NUMBER into the buffer."
   (interactive "NBoard number: ")
-  (add-to-list 'bridge-mode-callback-queue 'bridge-mode-insert-board t (lambda (_ _) nil))
-  (process-send-string nil (format "%s\n" (list 'deal number))))
+  (bridge-mode-send-request (list 'deal number) 'bridge-mode-insert-board))
 
 (provide 'bridge-mode)
 ;;; bridge-mode.el ends here
