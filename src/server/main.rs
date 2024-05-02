2b3053e0ec782d41e78be867cef98751ac637536
Sventimir <svantimir@gmail.com>
2024-05-02 06:55:55 +0200

    Mark server's responses with request's hash for identification.

--- src/server/main.rs
+++ src/server/main.rs
@@ -2,6 +2,8 @@ extern crate bridge;
 #[macro_use]
 mod command;
 
+use hex::ToHex;
+use ring::digest::{Digest, SHA256, digest};
 use serde::Serialize;
 use std::io;
 
@@ -14,10 +16,36 @@ enum ServerError {
     SexprError(String)
 }
 
-fn interpret(expr: &str) -> Result<CommandResult, ServerError> {
-    let cmd = serde_sexpr::from_str::<Cmd>(&expr)
-        .map_err(|e| ServerError::SexprError(e.to_string()))?;
-    cmd.execute().map_err(ServerError::CommandError)
+#[derive(Debug)]
+struct Response {
+    result: Result<CommandResult, ServerError>,
+    request_id: Digest
+}
+
+impl Serialize for Response {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer
+    {
+        use serde::ser::SerializeStruct;
+        let mut state = serializer.serialize_struct("Response", 2)?;
+        let req_id: String = ToHex::encode_hex(&self.request_id);
+        state.serialize_field("request_id", &req_id)?;
+        match &self.result {
+            Ok(result) => state.serialize_field("ok", result)?,
+            Err(err) => state.serialize_field("error", err)?,
+        }
+        state.end()
+    }
+}
+
+fn interpret(expr: &str) -> Response {
+    let request_id = digest(&SHA256, expr.as_bytes());     
+    let result = 
+        serde_sexpr::from_str::<Cmd>(&expr)
+        .map_err(|e| ServerError::SexprError(e.to_string()))
+        .and_then(|cmd| cmd.execute().map_err(ServerError::CommandError));
+    Response { result, request_id }
 }
 
 fn main() -> Result<(), String> {
