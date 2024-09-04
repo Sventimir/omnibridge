9f1feb7f1708403c930a8f06bf1a8a9b2cf1dccb
Sventimir <svantimir@gmail.com>
2024-09-04 11:54:15 +0200

    Add Conjunction operation to the runtime.

--- language/src/program.rs
+++ language/src/program.rs
@@ -1,48 +1,129 @@
+use std::fmt::{self, Debug, Formatter};
+use std::sync::{Arc, Mutex};
 
+pub struct Var(Arc<Mutex<u8>>);
 
-pub struct Program<'a> {
-    instructions: Vec<Instr<'a>>,
+impl Var {
+    pub fn new(val: u8) -> Self {
+        Var(Arc::new(Mutex::new(val)))
+    }
+
+    fn clone(&self) -> Self {
+        Self(Arc::clone(&self.0))
+    }
+
+    fn lock(&self) -> std::sync::MutexGuard<u8> {
+        self.0.lock().unwrap()
+    }
+
+    pub fn value(&self) -> u8 {
+        *self.lock()
+    }
 }
 
-pub enum Instr<'a> {
-    Not { arg: &'a u8, result: u8 },
+#[derive(Debug)]
+pub struct Program {
+    instructions: Arc<Mutex<Vec<Instr>>>,
 }
 
-impl<'a> Program<'a> {
+pub enum Instr {
+    Not { arg: Var, result: Var },
+    And { left: Var, right: Var, result: Var },
+}
+
+impl Program {
     pub fn new() -> Self {
         Program {
-            instructions: Vec::new(),
+            instructions: Arc::new(Mutex::new(Vec::new())),
         }
     }
 
-    pub fn exec(&mut self) {
-        for instr in &mut self.instructions {
+    pub fn exec(&self) -> Var {
+        let mut instrs = self.instructions.lock().unwrap();
+        for instr in &mut *instrs {
             instr.exec();
         }
-        self.result().unwrap();
+        instrs.last().unwrap().result()
     }
 
-    pub fn push_instr(&mut self, instr: Instr<'a>) {
-        self.instructions.push(instr);
+    pub fn push_instr(&self, instr: Instr) {
+        let mut instrs = self.instructions.lock().unwrap();
+        (*instrs).push(instr);
     }
 
-    pub fn result(&mut self) -> Option<&u8> {
-        self.instructions.last().map(Instr::result)
+    pub fn push_instr_not(&self, arg: &Var) -> Var {
+        let result = Var::new(123);
+        self.push_instr(Instr::Not {
+            arg: arg.clone(),
+            result: result.clone(),
+        });
+        result
+    }
+
+    pub fn push_instr_and(&self, left: &Var, right: &Var) -> Var {
+        let result = Var::new(18);
+        self.push_instr(Instr::And {
+            left: left.clone(),
+            right: right.clone(),
+            result: result.clone(),
+        });
+        result
+    }
+
+    pub fn result(&self) -> Option<Var> {
+        let instrs = self.instructions.lock().unwrap();
+        (*instrs).last().map(Instr::result)
     }
 }
 
-impl<'a> Instr<'a> {
+impl Instr {
     pub fn exec(&mut self) {
         match self {
-            Instr::Not { arg, ref mut result } => {
-                *result = !*arg;
+            Instr::Not { arg, result } => {
+                let mut ret = result.lock();
+                *ret = !*arg.lock();
+            }
+            Instr::And {
+                left,
+                right,
+                result,
+            } => {
+                let mut ret = result.lock();
+                *ret = *left.lock() & *right.lock();
             }
         }
     }
 
-    fn result(&self) -> &u8 {
+    fn result(&self) -> Var {
         match self {
-            Instr::Not { result, .. } => result,
+            Instr::Not { result, .. } | Instr::And { result, .. } => result.clone(),
         }
+    }
+}
+
+impl Debug for Instr {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Instr::Not { arg, result } => write!(f, "Not({:?} -> {:?})", arg.lock(), result.lock()),
+            Instr::And {
+                left,
+                right,
+                result,
+            } => {
+                write!(
+                    f,
+                    "And({:?}, {:?} -> {:?})",
+                    left.lock(),
+                    right.lock(),
+                    result.lock()
+                )
+            }
+        }
+    }
+}
+
+impl Debug for Var {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "Var({:?})", *self.lock())
     }
 }
--- language/src/test/runtime.rs
+++ language/src/test/runtime.rs
@@ -1,11 +1,22 @@
-use crate::program::{Instr, Program};
+use crate::program::{Program, Var};
 
 #[test]
 fn not_instr_flips_bool() {
-    let mut prog = Program::new();
-    let arg: u8 = 0;
-    let instr = Instr::Not { arg: &arg, result: 0 };
-    prog.push_instr(instr);
+    let prog = Program::new();
+    let arg: Var = Var::new(0);
+    let result = prog.push_instr_not(&arg);
     prog.exec();
-    assert_eq!(*prog.result().unwrap(), 255);
+    assert_eq!(result.value(), 255);
+}
+
+#[test]
+fn instrs_can_depend_on_each_ther() {
+    let prog = Program::new();
+    let arg1 = Var::new(0);
+    let arg2 = Var::new(255);
+    let neg_arg1 = prog.push_instr_not(&arg1);
+    let result = prog.push_instr_and(&neg_arg1, &arg2);
+    prog.exec();
+    println!("{:?}", prog);
+    assert_eq!(result.value(), 255);
 }
