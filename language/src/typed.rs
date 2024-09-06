0059e5b71a54a96977dc6d63b0d9830e0f5e3845
Sventimir <svantimir@gmail.com>
2024-09-06 12:40:04 +0200

    Remove unused code.

--- language/src/typed.rs
+++ language/src/typed.rs
@@ -2,8 +2,6 @@ pub trait Type: Copy {
     type Repr: Clone + Default; // Rust type representing this type
 
     fn symbol() -> &'static str;
-    fn to_bytes(repr: &Self::Repr) -> Vec<u8>;
-    fn from_bytes(bytes: Vec<u8>) -> Result<Self::Repr, MalformedDataError>;
 }
 
 #[derive(Clone, Copy)]
@@ -15,32 +13,8 @@ impl Type for Bool {
     fn symbol() -> &'static str {
         "bool"
     }
-
-    fn to_bytes(repr: &Self::Repr) -> Vec<u8> {
-        vec![*repr as u8]
-    }
-
-    fn from_bytes(bytes: Vec<u8>) -> Result<Self::Repr, MalformedDataError> {
-        if bytes.len() != 1 {
-            return Err(MalformedDataError {
-                typ: Self::symbol(),
-                bytes,
-            });
-        }
-        match bytes[0] & 1 {
-            0 => Ok(false),
-            1 => Ok(true),
-            _ => unreachable!(),
-        }
-    }
 }
 
 pub fn match_types<A: Type, B: Type>(_: A, _: B) -> bool {
     A::symbol() == B::symbol()
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct MalformedDataError {
-    typ: &'static str,
-    bytes: Vec<u8>,
 }
